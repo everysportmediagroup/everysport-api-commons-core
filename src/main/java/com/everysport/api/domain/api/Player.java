ee9368f7c32efc7b891ee1ec721c055c8bd780da
Mattias Eriksson <mattias.eriksson@menmo.se>
2020-02-27 15:58:34 +0100

    Support for player id for game events and lineups from Scoreradar

--- src/main/java/com/everysport/api/domain/api/Player.java
+++ src/main/java/com/everysport/api/domain/api/Player.java
@@ -1,13 +1,27 @@
 package com.everysport.api.domain.api;
 
 public class Player extends ApiBaseEntity {
+    private Integer id;
     private String name;
 
     public Player() {
     }
 
+    public Player(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Player(String name) {
         this.name = name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     public String getName() {
