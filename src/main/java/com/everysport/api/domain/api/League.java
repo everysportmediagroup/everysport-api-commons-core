3afc9614baae9b5d7ded47cd33c064f5eae8a9c7
Mattias Eriksson <mattias.eriksson@menmo.se>
2020-10-02 14:58:22 +0200

    EIR-629 Tagga serier med fakta-nivå och inkludera det i Everysport API

--- src/main/java/com/everysport/api/domain/api/League.java
+++ src/main/java/com/everysport/api/domain/api/League.java
@@ -29,6 +29,7 @@ public class League extends ApiNamedEntity {
     private List<Team> teams;	
 
     private Boolean liveScore;
+    private List<String> liveScoreEventData;
     private String years;
 
     public Integer getImmutableId() {
@@ -147,5 +148,13 @@ public class League extends ApiNamedEntity {
     public Integer getTeamClassId()
     {
         return teamClassId;
+    }
+
+    public List<String> getLiveScoreEventData() {
+        return liveScoreEventData;
+    }
+
+    public void setLiveScoreEventData(List<String> liveScoreEventData) {
+        this.liveScoreEventData = liveScoreEventData;
     }
 }
