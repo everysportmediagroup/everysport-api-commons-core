301d908433c6726b7883d73a890216583f61306e
Mattias Eriksson <mattias.eriksson@menmo.se>
2022-02-22 12:58:03 +0100

    sport slug

--- src/main/java/com/everysport/api/domain/api/Sport.java
+++ src/main/java/com/everysport/api/domain/api/Sport.java
@@ -3,4 +3,14 @@ package com.everysport.api.domain.api;
 public class Sport extends ApiNamedEntity {
     /* Used by Reflection in ResourceLinkUtils */
     public static final String RESOURCE_PATH = "/sports";
+
+    private String slug;
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
 }
