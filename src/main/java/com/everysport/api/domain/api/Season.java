e99a3d7498fe770f39241d664d42b8bdecd51ec1
Mattias Eriksson <mattias.eriksson@menmo.se>
2022-03-14 08:13:08 +0100

    DATA-207 Säsong ska taggas i api:et

--- src/main/java/com/everysport/api/domain/api/Season.java
+++ src/main/java/com/everysport/api/domain/api/Season.java
@@ -1,11 +1,13 @@
 package com.everysport.api.domain.api;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Season implements Serializable {
 
     private String name;
-    private static final long serialVersionUID = -7293115954664886344L;
+    private Integer startYear;
+    private Integer endYear;
 
     public String getName() {
         return name;
@@ -15,20 +17,34 @@ public class Season implements Serializable {
         this.name = name;
     }
 
+    public Integer getStartYear() {
+        return startYear;
+    }
+
+    public void setStartYear(Integer startYear) {
+        this.startYear = startYear;
+    }
+
+    public Integer getEndYear() {
+        return endYear;
+    }
+
+    public void setEndYear(Integer endYear) {
+        this.endYear = endYear;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         Season season = (Season) o;
-
-        if (name != null ? !name.equals(season.name) : season.name != null) return false;
-
-        return true;
+        return name.equals(season.name) &&
+                startYear.equals(season.startYear) &&
+                endYear.equals(season.endYear);
     }
 
     @Override
     public int hashCode() {
-        return name != null ? name.hashCode() : 0;
+        return Objects.hash(name, startYear, endYear);
     }
 }
