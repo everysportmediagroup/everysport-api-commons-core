5664b7dca569231aba99cc0f1ff957264e88a00e
Mattias Eriksson <mattias.eriksson@menmo.se>
2024-02-09 12:56:08 +0100

    updated submodule

--- src/main/java/com/everysport/api/domain/api/TeamStats.java
+++ src/main/java/com/everysport/api/domain/api/TeamStats.java
@@ -5,6 +5,8 @@ public class TeamStats extends ApiBaseEntity
 	private String groupType;
 
 	private String name;
+	private String label;
+	private String description;
 
 	private String value;
 
@@ -37,6 +39,22 @@ public class TeamStats extends ApiBaseEntity
 	public void setName(String name)
 	{
 		this.name = name;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public String getValue()
