795651441dd0f74002653434814c697c096041eb
Mattias Eriksson <mattias.eriksson@menmo.se>
2024-06-26 11:16:00 +0200

    DATA-486 Use of conference ID:s in Everysport API

---
+++ src/main/java/com/everysport/api/domain/api/SubLeague.java
@@ -0,0 +1,13 @@
+package com.everysport.api.domain.api;
+
+public class SubLeague extends ApiBaseEntity {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
